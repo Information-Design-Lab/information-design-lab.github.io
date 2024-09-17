4dd6a1582595a1ddb65f08d8388f137ce3616b4e
Daniele Ciminieri <daniele.ciminieri@gmail.com>
2024-09-17 18:07:16 +0200

    fix minor on mobile

--- src/lib/components/ProjectDetails.svelte
+++ src/lib/components/ProjectDetails.svelte
@@ -7,13 +7,17 @@
 		<h5 class="text-uppercase">{project.titolo}</h5>
 	</div>
 	<div class="col-md-6 col-sm-12 d-flex flex-row justify-content-between">
-		{#if project.studenti && project.studenti.length > 1}
-			<p class="fs-5">Progetto collettivo</p>
-		{:else if project.studenti}
-			{#each project.studenti as student}
-				<p class="fs-5">{student.nome} {student.cognome}</p>
-			{/each}
-		{/if}
-		<p class="fs-5 text-secondary">{project.tipologia}</p>
+		<div class="d-flex flex-column flex-md-row justify-content-between w-100">
+			<div>
+				{#if project.studenti && project.studenti.length > 1}
+					<p class="fs-5">Progetto collettivo</p>
+				{:else if project.studenti}
+					{#each project.studenti as student}
+						<p class="fs-5">{student.nome} {student.cognome}</p>
+					{/each}
+				{/if}
+			</div>
+			<p class="fs-5 text-secondary">{project.tipologia}</p>
+		</div>
 	</div>
 </div>
