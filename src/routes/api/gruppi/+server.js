4d4dbb95b722cf206b7e8f5b5605a0983e534dee
Daniele Ciminieri <daniele.ciminieri@gmail.com>
2024-09-18 15:11:17 +0200

    add json import

--- src/routes/api/gruppi/+server.js
+++ src/routes/api/gruppi/+server.js
@@ -1,5 +1,6 @@
 import { client } from '$lib/sanityClient';
 import { getGruppi } from '$lib/sanityQueries';
+import { error, json } from '@sveltejs/kit';
 
 export const prerender = true;
 export async function GET({ params, setHeaders, fetch }) {
