00527fe19fee07ad58fc0d53e6f9c96037c1b9f6
Daniele Ciminieri <daniele.ciminieri@gmail.com>
2024-11-15 17:50:44 +0100

    add social media sharing content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,18 @@
 	import Header from '$lib/components/Header.svelte';
 </script>
 
+<svelte:head>
+	<meta property="og:title" content="Information design lab" />
+	<meta property="og:type" content="website" />
+	<meta property="og:URL" content="hhttps://information-design-lab.github.io/" />
+	<meta property="og:image" content="https://information-design-lab.github.io/social-media.png" />
+	<meta
+		property="og:description"
+		content="
+Il laboratorio di Information Design è un corso del Master in Interaction Design dell'Università di San Marino che ha l'obiettivo di introdurre gli studenti ai concetti teorici e pratici realtivi alla visualizzazione dei dati e delle informazioni."
+	/>
+</svelte:head>
+
 <div
 	style="min-height: 100vh;"
 	class="container-fluid p-0 p-sm-0 d-flex flex-column justify-content-between"
